c21c77968abf2e39dca0de63e8f2e26085e649b4
solvedDev <33347616+solvedDev@users.noreply.github.com>
2019-01-06 19:13:58 +0100

    added solved.utilities.bridge_plugin_creator.js

---
+++ plugins/solved.utilities.bridge_plugin_creator.js
@@ -0,0 +1,24 @@
+Bridge.registerPlugin({
+    author: "solvedDev",
+    version: "1.1.0",
+    name: "Bridge Plugin Creator",
+    description: "Adds bridge. plugin files to bridge.'s native file creation window."
+});
+
+Bridge.BuildableFile.register({
+    options: {
+        extension: "js",
+        display_name: "Bridge Plugin",
+        path: "bridge/plugins/"
+    },
+    sidebar_element: {
+        title: "Bridge Plugin",
+        icon: "mdi-bridge"
+    },
+    templates: [
+        {
+            display_name: "Plugin start",
+            content: "// More on plugins: \"https://github.com/solvedDev/bridge./blob/master/plugins/getting-started.md\"\n\nBridge.registerPlugin({\n\tauthor: \"\",\n\tversion: \"v1.0.0\",\n\tname: \"\",\n\tdescription: \"\"\n});"
+        }
+    ]
+});
