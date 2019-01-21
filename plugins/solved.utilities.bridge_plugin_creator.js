afd736630756480caa352f18b10b65c54322639a
solvedDev <33347616+solvedDev@users.noreply.github.com>
2019-01-21 22:22:01 +0100

    Update solved.utilities.bridge_plugin_creator.js

--- plugins/solved.utilities.bridge_plugin_creator.js
+++ plugins/solved.utilities.bridge_plugin_creator.js
@@ -1,5 +1,5 @@
 Bridge.registerPlugin({
-    author: "bridge. Team",
+    author: "solvedDev",
     version: "1.0.0",
     name: "Bridge Plugin Creator",
     description: "Adds bridge. plugin files to bridge.'s native file creation window."
@@ -17,7 +17,7 @@ Bridge.BuildableFile.register({
     },
     templates: [
         {
-            display_name: "Plugin start",
+            display_name: "Plugin Start",
             content: "// More on plugins: \"https://github.com/solvedDev/bridge./blob/master/plugins/getting-started.md\"\n\nBridge.registerPlugin({\n\tauthor: \"\",\n\tversion: \"v1.0.0\",\n\tname: \"\",\n\tdescription: \"\"\n});"
         }
     ]
