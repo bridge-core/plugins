fd8f33514df727123dafadc2c439711d045fcaf3
solvedDev <solvedDev@gmail.com>
2021-02-01 12:21:22 +0100

    v2plugins.json > extensions.json

--- scripts/build.ts
+++ scripts/build.ts
@@ -2,7 +2,7 @@ import { join } from 'https://deno.land/std@0.85.0/path/mod.ts'
 import { JSZip } from 'https://raw.githubusercontent.com/hayd/deno-zip/master/mod.ts'
 
 const v1Plugins = []
-const v2Plugins = []
+const extensions = []
 
 for await (const dirEntry of Deno.readDir('./plugins')) {
 	if (dirEntry.isFile) continue
@@ -38,9 +38,9 @@ for await (const dirEntry of Deno.readDir('./plugins')) {
 	manifest.link = `/plugins/${dirEntry.name}/plugin.zip`
 
 	if (manifest.target === 'both') {
-		v2Plugins.push(manifest)
+		extensions.push(manifest)
 		v1Plugins.push(manifest)
-	} else if (manifest.target === 'v2') v2Plugins.push(manifest)
+	} else if (manifest.target === 'v2') extensions.push(manifest)
 	else v1Plugins.push(manifest)
 }
 
@@ -59,4 +59,4 @@ async function readDirectory(zip: JSZip, path: string) {
 }
 
 await Deno.writeTextFile('./plugins.json', JSON.stringify(v1Plugins))
-await Deno.writeTextFile('./v2Plugins.json', JSON.stringify(v2Plugins))
+await Deno.writeTextFile('./extensions.json', JSON.stringify(extensions))
