328a160b46986b3ca2f8afdbfc09b2586067de1a
aexer0e <aexeroe@gmail.com>
2021-05-15 13:49:27 +0300

    Added Plugin: 'Compact Prettier'

---
+++ plugins/CompactPrettier/scripts/compactPrettier.js
@@ -0,0 +1,107 @@
+// ass
+
+const { registerDocumentFormattingEditProvider } = await require('@bridge/monaco');
+const { parse } = await require('@bridge/json5');
+
+// Register the formatting provider
+registerDocumentFormattingEditProvider('json', {
+	provideDocumentFormattingEdits(model, options, token) {
+		return [
+			{
+				text: formatText(model.getValue()),
+				range: model.getFullModelRange(),
+			},
+		]
+	},
+});
+
+// src is of type string here because model.getValue() returns a string
+function formatText(src) {
+	if (parse(src)) {
+		function stringify(passedObj, options) {
+			let stringOrChar = /("(?:[^\\"]|\\.)*")|[:,]/g;
+			var indent, maxLength, replacer;
+			options = options || {};
+			indent = JSON.stringify(
+				[1],
+				undefined,
+				options.indent === undefined ? 2 : options.indent
+			).slice(2, -3);
+			maxLength =
+				indent === ""
+					? Infinity
+					: options.maxLength === undefined
+						? 80
+						: options.maxLength;
+			replacer = options.replacer;
+			return (function _stringify(obj, currentIndent, reserved) {
+				var end, index, items, key, keyPart, keys, length, nextIndent, prettified, start, string, value;
+				if (obj && typeof obj.toJSON === "function") {
+					obj = obj.toJSON();
+				}
+				string = JSON.stringify(obj, replacer);
+				if (string === undefined) {
+					return string;
+				}
+				length = maxLength - currentIndent.length - reserved;
+				if (string.length <= length) {
+					prettified = string.replace(stringOrChar, function (match, stringLiteral) {
+						return stringLiteral || match + " ";
+					});
+					if (prettified.length <= length) {
+						return prettified;
+					}
+				}
+				if (replacer != null) {
+					obj = JSON.parse(string);
+					replacer = undefined;
+				}
+				if (typeof obj === "object" && obj !== null) {
+					nextIndent = currentIndent + indent;
+					items = [];
+					index = 0;
+					if (Array.isArray(obj)) {
+						start = "[";
+						end = "]";
+						length = obj.length;
+						for (; index < length; index++) {
+							items.push(
+								_stringify(obj[index], nextIndent, index === length - 1 ? 0 : 1) ||
+								"null"
+							);
+						}
+					} else {
+						start = "{";
+						end = "}";
+						keys = Object.keys(obj);
+						length = keys.length;
+						for (; index < length; index++) {
+							key = keys[index];
+							keyPart = JSON.stringify(key) + ": ";
+							value = _stringify(
+								obj[key],
+								nextIndent,
+								keyPart.length + (index === length - 1 ? 0 : 1)
+							);
+							if (value !== undefined) {
+								items.push(keyPart + value);
+							}
+						}
+					}
+					if (items.length > 0) {
+						return [start, indent + items.join(",\n" + nextIndent), end].join(
+							"\n" + currentIndent
+						);
+					}
+				}
+				return string;
+			})(passedObj, "", 0);
+		};
+
+		let data_out = stringify(parse(src), { maxLength: 150, indent: 4, });
+		data_out = data_out.replace(/(?<={|\[)(?="|[0-9]|-)|(?<=[0-9]|")(?=}|\])/g, ' ');
+
+		return data_out
+	}
+	else { return src }
+}
