81605b892298f36e505b3ae7f4fddc6031b0324f
SineVector241 <69619913+SineVector241@users.noreply.github.com>
2021-08-16 10:56:32 +1000

    Updated Extension
    Fixed the id in docs.js incase any other extensions overwrite it.

--- plugins/MinecraftDocs/scripts/Docs.js
+++ plugins/MinecraftDocs/scripts/Docs.js
@@ -2,7 +2,7 @@ const { SideBar } = await require('@bridge/ui')
 const { create } = await require('@bridge/sidebar')
 
 create({
-	id: 'myExtension',
+	id: 'MinecraftDocumentation',
 	displayName: 'Minecraft Documentations',
 	icon: 'mdi-file-document-multiple',
 	component: SideBar
