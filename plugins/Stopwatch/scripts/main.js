684efc992ae225356cc656aeebcd5896623b431c
Pascal <pascal.de.heij12@gmail.com>
2021-08-23 12:30:12 +0200

    Add Stopwatch extension

---
+++ plugins/Stopwatch/scripts/main.js
@@ -0,0 +1,9 @@
+const { create } = await require('@bridge/sidebar')
+const { Main } = await require('@bridge/ui')
+
+const sidebar = create({
+    id: 'pascal.stopwatch.sidebar',
+    icon: 'mdi-timer',
+    displayName: 'Stopwatch',
+    component: Main,
+})
---
+++ plugins/Stopwatch/ui/Main.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <v-container>
+      <v-row justify="center">
+        <h1>Stopwatch</h1>
+      </v-row>
+      <v-divider/>
+      <v-row justify="center">
+        <h1>{{ this.formattedTime }}</h1>
+      </v-row>
+      <v-divider/>
+      <v-row>
+        <v-col>
+          <v-btn color="primary" block @click="onToggle">
+            <v-icon v-if="!isRunning">mdi-play</v-icon>
+            <v-icon v-else>mdi-pause</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col>
+          <v-btn color="primary" block @click="resetTime">
+            <v-icon>mdi-restart</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+const {readJSON, writeJSON} = await require('@bridge/fs')
+const {join} = await require('@bridge/path')
+const {getCurrentProject} = await require('@bridge/env')
+
+let filePath = join(getCurrentProject(), '.bridge/stopwatch.json')
+
+export default {
+  async mounted() {
+    try {
+      let data = await readJSON(filePath)
+      Object.assign(this.$data, data)
+
+      if (data.isRunning) {
+        this.startTime()
+      } else {
+        this.formatTime()
+      }
+    } catch {
+    }
+  },
+  data() {
+    return {
+      timer: null,
+      msTime: 0,
+      formattedTime: "00:00:00.00",
+      isRunning: false,
+      startTimestamp: null
+    }
+  },
+  methods: {
+    onToggle() {
+      if (this.isRunning) {
+        this.isRunning = false
+        this.pauseTime()
+      } else {
+        this.isRunning = true
+        this.startTimestamp = Date.now() - this.msTime
+        this.startTime()
+      }
+      this.writeData()
+    },
+    startTime() {
+      this.timer = setInterval(() => {
+        this.updateTime()
+      }, 10)
+    },
+    pauseTime() {
+      clearInterval(this.timer)
+    },
+    resetTime() {
+      this.msTime = 0
+      this.startTimestamp = Date.now()
+      this.formatTime()
+      this.writeData()
+    },
+    updateTime() {
+      this.msTime = Date.now() - this.startTimestamp
+      this.formatTime()
+    },
+    formatTime() {
+      this.formattedTime = new Date(this.msTime).toISOString().slice(11, -2)
+    },
+    async writeData() {
+      await writeJSON(filePath, this.$data)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
