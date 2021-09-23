f4e9b6c9b888d5211c243049a23932a19a5366eb
Joelant05 <Joelant05@users.noreply.github.com>
2021-09-23 20:48:28 +0100

    upd: ItemEquippedSensor v1.0.2

--- plugins/ItemEquippedSensorV2/components/item/itemEquippedSensor.js
+++ plugins/ItemEquippedSensorV2/components/item/itemEquippedSensor.js
@@ -1,14 +1,18 @@
 export default function defineComponent({ name, template, schema }) {
+	// TODO: Apply some sort of versioning to is_(un)equipped component schema reference
 	name('bridge:item_equipped_sensor')
 	schema({
+		description: 'Allows you to run commands, player events and apply components to the player when the item is equipped/unequipped in specified slots.',
+		additonalProperties: false,
 		properties: {
 			sensors: {
 				type: 'array',
+				description: 'List of sensors to detect this item.',
 				items: {
 					properties: {
 						on_equip: {
 							description:
-								'List events and commands to be run on the player when the item is equipped',
+								'List events and commands to be run on the player when the item is equipped.',
 							type: 'array',
 							items: {
 								$ref:
@@ -17,7 +21,7 @@ export default function defineComponent({ name, template, schema }) {
 						},
 						on_unequip: {
 							description:
-								'List events and commands to be run on the player when the item is unequipped',
+								'List events and commands to be run on the player when the item is unequipped.',
 							type: 'array',
 							items: {
 								$ref:
@@ -26,18 +30,18 @@ export default function defineComponent({ name, template, schema }) {
 						},
 						is_equipped: {
 							description:
-								'Components to be applied to the player while the item is equipped',
+								'Components to be applied to the player while the item is equipped.',
 							$ref:
-								'/data/packages/minecraftBedrock/schema/entity/v1.16.200/components.json',
+								'/data/packages/minecraftBedrock/schema/entity/v1.17.20/components/_main.json',
 						},
 						is_unequipped: {
 							description:
-								'Components to be applied to the player while the item is unequipped',
+								'Components to be applied to the player while the item is unequipped.',
 							$ref:
-								'/data/packages/minecraftBedrock/schema/entity/v1.16.200/components.json',
+								'/data/packages/minecraftBedrock/schema/entity/v1.17.20/components/_main.json',
 						},
 						slot: {
-							description: 'Set the slot to check for this item',
+							description: 'Set the slot to check for this item.',
 							$ref:
 								'/data/packages/minecraftBedrock/schema/general/slotType.json',
 						},
