805907914b44292bb4459f347b23095f3c6d16fd
Joelant05 <64587014+Joelant05@users.noreply.github.com>
2022-07-02 17:12:09 +0100

    VanillaPackViewer v1.1.0

--- plugins/VanillaPackViewer/scripts/main.js
+++ plugins/VanillaPackViewer/scripts/main.js
@@ -2,8 +2,11 @@ const { create, SidebarContent, SelectableSidebarAction } =
 	await require('@bridge/sidebar')
 const { VanillaPackViewer, Header } = await require('@bridge/ui')
 const { addFolderImporter, importHandle } = await require('@bridge/import')
-const { getDirectoryHandle, onBridgeFolderSetup } = await require('@bridge/fs')
 const { createInformationWindow } = await require('@bridge/windows')
+const { readFilesFromDir, getFileHandle } = await require('@bridge/fs')
+const storage = await require('@bridge/persistent-storage')
+const { registerAction } = await require('@bridge/command-bar')
+const { openFile } = await require('@bridge/project')
 
 class VanillaPackSidebarContent extends SidebarContent {
 	headerSlot = Header
@@ -13,29 +16,30 @@ class VanillaPackSidebarContent extends SidebarContent {
 		vanillaBehaviorPack: null,
 		vanillaResourcePack: null,
 	}
-	isForeignPack = false
+	hasHandleAccess = false
+	disposables = []
 
 	constructor() {
 		super()
 
-		onBridgeFolderSetup(async () => {
-			await this.setup()
-		})
+		// Add the option to import folder as vanilla pack
 		addFolderImporter({
 			icon: 'mdi-link-variant',
 			name: '[Vanilla Pack]',
 			description:
 				'[Link a vanilla pack for it to be viewable inside of bridge.]',
-			onSelect: async (dirHandle) =>
-				await this.handleFolderImport(dirHandle),
+			onSelect: async (dirHandle) => {
+				await this.handleFolderImport(dirHandle)
+			},
 		})
-		this.setup()
-
+		// Load existing handles from idb
+		this.loadHandles()
+		// Set height of sidebar header UI
 		this.headerHeight = '60px'
 	}
 
 	async handleFolderImport(dirHandle) {
-		// Decide whether it is the vanilla BP or RP from manifest
+		// Decide whether it is the vanilla BP or RP by looking at manifest in directory and reading module type
 		if (dirHandle.kind === 'directory') {
 			let type
 			for await (const handle of dirHandle.values()) {
@@ -59,45 +63,76 @@ class VanillaPackSidebarContent extends SidebarContent {
 			}
 
 			if (!type) {
-				// Not a valid VP
-				console.log(`No VP type found in manifest in ${dirHandle.name}`)
+				// If type couldn't be decided, show warning
+				createInformationWindow(
+					'[Vanilla Packs]',
+					`[Could not recognise pack as there is no valid manifest in the "${dirHandle.name}" directory.]`
+				)
 			} else {
-				// Is valid
+				// If it was decided, continue...
+				// Load existing idb data
+				const data = (await storage.load()) ?? {
+					directoryEntries: {
+						vanillaBehaviorPack: null,
+						vanillaResourcePack: null,
+					},
+				}
+				// Set behavior pack
 				if (type === 'behaviorPack') {
-					this.addPack('behaviorPack', dirHandle, true)
-					this.isForeignPack = true
+					data.directoryEntries.vanillaBehaviorPack = dirHandle
 				}
-				if (type === 'resourcePack') {
-					this.addPack('resourcePack', dirHandle, true)
-					this.isForeignPack = true
+				// OR
+				// Set resource pack
+				else if (type === 'resourcePack') {
+					data.directoryEntries.vanillaResourcePack = dirHandle
 				}
+				// Save these new file handles to idb and set them on the sidebar state for DirectoryViewer
+				await storage.save(data)
+				this.directoryEntries = data.directoryEntries
+				// Setup viewer
 				await this.setup()
 			}
 		} else {
+			// Show warning if imported pack wasn't a directory (e.g. a zip file)
 			createInformationWindow(
-				'[Invalid]',
+				'[Vanilla Packs]',
 				'[Please try again with a valid vanilla behavior or resource pack folder.]'
 			)
 		}
 	}
 
+	async loadHandles() {
+		// Load data from idb
+		const data = await storage.load()
+		// Set directory entries if they were saved
+		if (data?.directoryEntries)
+			this.directoryEntries = data.directoryEntries
+	}
+
 	async setup() {
-		if (!this.isForeignPack) {
-			this.unselectAllActions()
-			this.actions = []
-			const dirHandle = await getDirectoryHandle('data/vanillaPacks', {
-				create: true,
-			})
-			for await (const handle of dirHandle.values()) {
-				if (handle.kind === 'directory') {
-					if (
-						handle.name === 'behaviorPack' ||
-						handle.name === 'resourcePack'
-					)
-						this.addPack(handle.name, handle)
-				}
-			}
-		}
+		// Get access to handles if necessary
+		await this.accessHandles()
+
+		// If we have a BP handle and access to it, add action to VP viewer
+		if (
+			this.directoryEntries.vanillaBehaviorPack &&
+			(await this.directoryEntries.vanillaBehaviorPack.queryPermission()) ===
+				'granted'
+		)
+			this.addPack(
+				'behaviorPack',
+				this.directoryEntries.vanillaBehaviorPack
+			)
+		// If we have an RP handle and access to it, add action to VP viewer
+		if (
+			this.directoryEntries.vanillaResourcePack &&
+			(await this.directoryEntries.vanillaResourcePack.queryPermission()) ===
+				'granted'
+		)
+			this.addPack(
+				'resourcePack',
+				this.directoryEntries.vanillaResourcePack
+			)
 
 		// Sort actions, remove resourcePack action and put on end
 		if (
@@ -111,20 +146,85 @@ class VanillaPackSidebarContent extends SidebarContent {
 		}
 	}
 
+	async accessHandles() {
+		// Check if we already have permissions for VP types
+		const bpPermission =
+			this.directoryEntries.vanillaBehaviorPack &&
+			(await this.directoryEntries.vanillaBehaviorPack.queryPermission()) ===
+				'granted'
+		const rpPermission =
+			this.directoryEntries.vanillaResourcePack &&
+			(await this.directoryEntries.vanillaResourcePack.queryPermission()) ===
+				'granted'
+
+		// If we already have permission for either, update sidebar to show DirectoryViewer and don't continue with requesting permissions
+		// Don't need to worry about which pack we specifically have access to because the sidebar action will not be registered if there is no access
+		if (bpPermission || rpPermission) {
+			this.hasHandleAccess = true
+			return
+		}
+
+		// For both VBP and VRP, if we don't already have access and the handle has been given to bridge. (either thorugh being dragged on or through idb storage)
+		// ...create promises for the permission prompts
+		const promises = []
+		if (this.directoryEntries.vanillaBehaviorPack && !bpPermission) {
+			promises.push(
+				this.directoryEntries.vanillaBehaviorPack.requestPermission({
+					mode: 'read',
+				})
+			)
+		}
+
+		if (this.directoryEntries.vanillaResourcePack && !rpPermission) {
+			promises.push(
+				await this.directoryEntries.vanillaResourcePack.requestPermission(
+					{
+						mode: 'read',
+					}
+				)
+			)
+		}
+		const res = await Promise.all(promises)
+		// If any permission prompt was granted, update sidebar property so that DirectoryViewer is shown
+		if (res.some((r) => r === 'granted')) this.hasHandleAccess = true
+	}
+
+	hasHandle() {
+		// Return whether bridge. has been given either a VBP or VRP handle
+		return !!(
+			this.directoryEntries.vanillaBehaviorPack ||
+			this.directoryEntries.vanillaResourcePack
+		)
+	}
+
+	async unlink() {
+		// Clear idb stored handles and remove handles from sidebar state so they aren't passed to DirectoryViewer
+		await storage.delete()
+		this.directoryEntries.vanillaResourcePack = null
+		this.directoryEntries.vanillaBehaviorPack = null
+
+		// Dispose of all command bar actions
+		for (const disposable of this.disposables) disposable.dispose()
+	}
+
 	addPack(type, dirHandle, showCannotLinkWarning = false) {
+		// Format name to action id, e.g. "behaviorPack" -> "vanillaBehaviorPack"
 		const formattedName = `vanilla${type[0].toUpperCase()}${type.substring(
 			1
 		)}`
+
+		// Check whether the pack already exists in actions, and show warning if necessary
 		for (const action of this.actions) {
 			if (action.config.id === formattedName) {
-				if (showCannotLinkWarning) createInformationWindow(
-					'[Vanilla Packs]',
-					'[Cannot add pack because a pack of this type is already linked to the vanilla pack viewer!]'
-				)
+				if (showCannotLinkWarning)
+					createInformationWindow(
+						'[Vanilla Packs]',
+						'[Cannot add pack because a pack of this type is already linked to the vanilla pack viewer!]'
+					)
 				return
 			}
 		}
-
+		// Push new action for VP to sidebar
 		this.actions.push(
 			new SelectableSidebarAction(this, {
 				id: formattedName,
@@ -135,10 +235,11 @@ class VanillaPackSidebarContent extends SidebarContent {
 				color: type,
 			})
 		)
-		this.directoryEntries[formattedName] = dirHandle
+		this.registerOpenFileActions(dirHandle, type)
 	}
 
 	getFileContextMenu(fileWrapper) {
+		// Get the custom file context menu to be appended to default, non-mutating context menu
 		return [
 			{ type: 'divider' },
 			{
@@ -146,12 +247,47 @@ class VanillaPackSidebarContent extends SidebarContent {
 				name: '[Import to Project]',
 				onTrigger: async () => {
 					await importHandle(fileWrapper.handle)
-				}
-			}
+				},
+			},
 		]
+	}
+
+	registerOpenFileActions(dirHandle, type) {
+		// Get every file from the pack folder
+		readFilesFromDir('', dirHandle).then((data) => {
+			// Iterate files
+			for (const file of data) {
+				this.disposables.push(
+					registerAction({
+						name: `[${type
+							.replace('behaviorPack', 'BP')
+							.replace(
+								'resourcePack',
+								'RP'
+							)}/${file.path.substring(1)}]`,
+						description: '[Open Vanilla File]',
+						icon: 'mdi-minecraft',
+						color: type,
+
+						onTrigger: async () => {
+							// Get file handle, relative to the dirHandle argument
+							const pathArr = file.path.substring(1).split(/\\|\//g)
+							const fileName = pathArr.pop()
+							let current = dirHandle
+							for (const folder of pathArr) {
+								current = await current.getDirectoryHandle(folder)
+							}
+							const fileHandle = await current.getFileHandle(fileName)
+							await openFile(fileHandle, { isReadOnly: true })
+						},
+					})
+				)
+			}
+		})
 	}
 }
 
+// Add the sidebar to the app
 create({
 	id: 'joelant05.bridge.vpviewer',
 	displayName: '[Vanilla Packs]',
--- plugins/VanillaPackViewer/ui/VanillaPackViewer.vue
+++ plugins/VanillaPackViewer/ui/VanillaPackViewer.vue
@@ -1,43 +1,56 @@
 <template>
-	<v-container
-		v-if="
-			!sidebarContent.directoryEntries.vanillaBehaviorPack &&
-			!sidebarContent.directoryEntries.vanillaResourcePack
-		"
-	>
-		<BridgeSheet
-			dark
-			class="mb-2 pa-2 d-flex flex-column"
-			style="overflow: hidden; text-align: center"
-			><v-icon style="font-size: 3rem" color="error" class="mb-4">
-				mdi-link-variant-off </v-icon
-			>It looks like you don't have a vanilla pack for bridge. to use!
-			Download the vanilla BP and RP and extract them. Then, outside of bridge. drag them into your bridge folder's<br />
-			<span style="font-weight: 550; color: var(--v-secondary-base)"
-				>data/vanillaPacks/behaviorPack</span
+	<v-container v-if="!sidebarContent.hasHandleAccess">
+		<div v-if="!sidebarContent.hasHandle()">
+			<BridgeSheet
+				dark
+				class="mb-2 pa-2 d-flex flex-column"
+				style="overflow: hidden; text-align: center"
+				><v-icon style="font-size: 3rem" color="error" class="mb-4">
+					mdi-link-variant-off </v-icon
+				>It looks like you don't have a vanilla pack linked for bridge.
+				to use! Download the vanilla BP and RP and extract them. Then,
+				drag the extracted folders onto bridge. to link
+				them.</BridgeSheet
 			>
-			and<br />
-			<span style="font-weight: 550; color: var(--v-secondary-base)"
-				>data/vanillaPacks/resourcePack</span
+			<v-btn
+				color="secondary"
+				class="mb-2 d-flex flex-column"
+				style="width: 100%"
+				@click="download"
+				><span>Download Vanilla Packs</span></v-btn
 			>
-			folders respectively.</BridgeSheet
-		>
-		<v-btn
-			color="secondary"
-			class="mb-2 d-flex flex-column"
-			style="width: 100%"
-			@click="download"
-			><span>Download Vanilla Packs</span></v-btn
-		>
-		<BridgeSheet
-			dark
-			class="mb-2 pa-2 d-flex flex-column"
-			style="overflow: hidden; text-align: center"
-			><v-icon style="font-size: 3rem" color="success" class="mb-4">
-				mdi-link-variant </v-icon
-			>Alternatively, you can temporarily link a vanilla pack by dragging
-			it onto bridge.</BridgeSheet
-		>
+		</div>
+
+		<div v-else>
+			<BridgeSheet
+				dark
+				class="mb-2 pa-2 d-flex flex-column"
+				style="overflow: hidden; text-align: center"
+				><v-icon style="font-size: 3rem" color="success" class="mb-4">
+					mdi-link-variant</v-icon
+				>
+				It looks like you have previously linked vanilla packs to
+				bridge.! To continue using these, grant bridge. permissions to
+				the folders. Alternatively, unlink the packs and add new
+				ones.</BridgeSheet
+			>
+			<v-btn
+				color="secondary"
+				class="mb-2 d-flex flex-column"
+				style="width: 100%"
+				@click="sidebarContent.setup()"
+				><v-icon>mdi-folder-open-outline</v-icon
+				><span>Access Linked Packs</span></v-btn
+			>
+			<v-btn
+				color="error"
+				class="mb-2 d-flex flex-column"
+				style="width: 100%"
+				@click="sidebarContent.unlink()"
+				><v-icon>mdi-link-variant-off</v-icon
+				><span>Unlink Packs</span></v-btn
+			>
+		</div>
 	</v-container>
 	<DirectoryViewer
 		v-else-if="sidebarContent.selectedAction"
@@ -49,7 +62,7 @@
 				.toLowerCase()}${sidebarContent.selectedAction.config.id
 				.replace('vanilla', '')
 				.substring(1)}`,
-			provideFileContextMenu: sidebarContent.getFileContextMenu
+			provideFileContextMenu: sidebarContent.getFileContextMenu,
 		}"
 		:directoryHandle="
 			sidebarContent.directoryEntries[
