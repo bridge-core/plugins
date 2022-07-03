c8b60e811892952bdac9288f9e29fb164e52fb20
solvedDev <solvedDev@gmail.com>
2022-07-03 12:52:26 +0200

    upd: Molang Playground v1.0.1

---
+++ plugins/MolangPlayground/scripts/tab.js
@@ -0,0 +1,66 @@
+const { IframeTab, addTab, getCurrentTabSystem } = await require('@bridge/tab')
+const { create } = await require('@bridge/sidebar')
+const { register, addTabActions } = await require('@bridge/tab-actions')
+const { openExternal } = await require('@bridge/utils')
+const commandBar = await require('@bridge/command-bar')
+
+class MolangPlaygroundTab extends IframeTab {
+	type = 'MolangPlaygroundTab'
+
+	async setup() {
+		addTabActions(this)
+
+		await super.setup()
+	}
+
+	static is() {
+		return false
+	}
+
+	get icon() {
+		return 'mdi-function-variant'
+	}
+	get iconColor() {
+		return 'primary'
+	}
+	get name() {
+		return 'Molang Playground'
+	}
+}
+
+async function createTab() {
+	await addTab(
+		new MolangPlaygroundTab(await getCurrentTabSystem(), {
+			url: 'https://bridge-core.app/molang-playground',
+		})
+	)
+}
+
+register({
+	icon: 'mdi-open-in-new',
+	name: '[Open New]',
+	trigger() {
+		openExternal('https://bridge-core.app/molang-playground/?molang=Q')
+	},
+	isFor(tab) {
+		return tab.type === 'MolangPlaygroundTab'
+	},
+})
+
+create({
+	id: 'solvedDev.bridge.molangPlayground',
+	displayName: '[Molang Playground]',
+	icon: 'mdi-function-variant',
+	onClick: async () => {
+		await createTab()
+	},
+})
+
+commandBar.registerAction({
+	icon: 'mdi-function-variant',
+	name: '[Open Molang Playground]',
+	description: '[Open a Molang Playground tab within bridge.]',
+	onTrigger: async () => {
+		await createTab()
+	},
+})
