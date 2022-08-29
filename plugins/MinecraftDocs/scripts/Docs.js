abec83b58a11ea3109299d07e61b59dd3e69614b
SineVector241 <69619913+SineVector241@users.noreply.github.com>
2022-08-29 12:09:13 +1000

    Delete plugins/MinecraftDocs directory

--- plugins/MinecraftDocs/scripts/Docs.js
+++
@@ -1,9 +0,0 @@
-const { SideBar } = await require('@bridge/ui')
-const { create } = await require('@bridge/sidebar')
-
-create({
-	id: 'MinecraftDocumentation',
-	displayName: 'Minecraft Documentations',
-	icon: 'mdi-file-document-multiple',
-	component: SideBar
-})
